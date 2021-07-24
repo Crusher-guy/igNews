4af5923d5d17d80db3ecdec458d80b78b9adcbfb
leokattah <leokattah@gmail.com>
2021-07-24 19:13:10 -0300

    global configs added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AppProps } from 'next/app';
+import '../styles/global.scss';
+
+//Tudo que eu quiser que se repita em todas as páginas, eu insiro no _app dentro da função Myapp. 
+//Toda vez que o usuário mudar de página, tudo que estiver aqui será recarregado, assim, deve-se ter cautela para inserir dados aqui para não prejudicar a performance. 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+ 
