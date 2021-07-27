1d2c8d04bec3c153402d326f4bfb691022e2774c
leokattah <leokattah@gmail.com>
2021-07-27 12:30:19 -0300

    cms and menu nav done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,27 @@
 //O Header estará visível em todas as páginas, então vou colocá-lo no _app.
+// O componente Link é o recurso que permite que as páginas sejam carregadas como SPA e não precisem carregar todo o conteúdo novamente. Assim, o Next preserva o benefício de react que é garantir boa performance.
 
-import {SignInButton} from '../SignInButton/index'
+import Link from 'next/link'
+import { SignInButton } from '../SignInButton/index'
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink'
 
-export function Header(){
-  return(
+
+export function Header() {
+
+  return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-         <img src="/images/logo.svg" alt="Logo"/>
-         <nav>
-           <a className={styles.active}>Home</a>
-           <a>Posts</a>
-         </nav>
-         <SignInButton />
+        <img src="/images/logo.svg" alt="Logo" />
+        <nav>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+        <SignInButton />
       </div>
     </header>
   )
