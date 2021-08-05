948a2a269f3e16273153b72db81b5a850aae64f2
leokattah <leokattah@gmail.com>
2021-08-05 20:54:51 -0300

    posts updated with prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,13 @@
 import { GetStaticProps } from 'next';
-import Head from 'next/head';
+import Head from 'next/head'
+
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
-import Link from 'next/link';
 
 import { getPrismicClient } from '../../services/prismic';
 
-import styles from './styles.module.scss';
+import styled from './styles.module.scss';
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -19,21 +20,22 @@ interface PostsProps {
   posts: Post[]
 }
 
-export default function Posts({ posts }: PostsProps) {
+export default function Posts ({ posts }: PostsProps) {
   return (
     <>
       <Head>
-        <title>Posts | Ignews</title>
+         <title>Posts | Ignews</title>
       </Head>
 
-      <main className={styles.container}>
-        <div className={styles.posts}>
+      <main className={styled.container}>
+        <div className={styled.posts}>
           { posts.map(post => (
-            <Link key={post.slug} href={`/posts/${post.slug}`}>
-              <a>
-                <time>{post.updatedAt}</time>
-                <strong>{post.title}</strong>
-                <p>{post.excerpt}</p>
+            // eslint-disable-next-line react/jsx-key
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{ post.updatedAt }</time>
+                <strong>{ post.title }</strong>
+                <p>{ post.excerpt }</p>
               </a>
             </Link>
           )) }
@@ -47,10 +49,10 @@ export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient()
 
   const response = await prismic.query([
-    Prismic.predicates.at('document.type', 'publication')
+    Prismic.predicates.at('document.type', 'post')
   ], {
-    fetch: ['publication.title', 'publication.content'],
-    pageSize: 100,
+   fetch: ['post.title', 'post.content'],
+   pageSize: 100,
   })
 
   const posts = response.results.map(post => {
@@ -61,10 +63,10 @@ export const getStaticProps: GetStaticProps = async () => {
       updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
         day: '2-digit',
         month: 'long',
-        year: 'numeric'
+        year: 'numeric',
       })
     };
-  });
+  })
 
   return {
     props: {
@@ -72,17 +74,3 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
